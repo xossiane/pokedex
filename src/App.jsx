15b780cea3e4683169fb431d721e6d237f91e888
xossiane <anejosi.lima@gmail.com>
2022-07-05 15:49:27 -0300

    Tentativa de salvar o merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
+import "../public/styles/Global.css";
 
+<<<<<<< HEAD
 
 import './App.css'
 import CardCategories from './components/molecules/CardCategories'
+import footer from '../public/assets/footer.jpg'
+=======
+import Home from "./components/pages/Home";
+import CardInfo from "./components/atoms/CardInfo";
 
+>>>>>>> main
 function App() {
-
   return (
-    <div className="App">
-      <header className="CardCategories_homepage">
-        <div>
-        <CardCategories></CardCategories>
-        </div>
-      </header>
+<<<<<<< HEAD
+    <div className="CardCategories__homepage__footer">
+    <div className="CardCategories__homepage">
+    <CardCategories></CardCategories>
+    
+    </div>
+    <img className="footer" src="/assets/footer.jpg"/>
     </div>
   )
+=======
+    <>
+      <Home />
+      <div className="App">
+        <header className="App-header">
+          <CardInfo></CardInfo>
+          <CardInfo></CardInfo>
+          <CardInfo></CardInfo>
+        </header>
+      </div>
+    </>
+  );
+>>>>>>> main
 }
 
-export default App
+export default App;
