33effcb4261141a7653566feea8bd5e9fc2b4c4c
xossiane <anejosi.lima@gmail.com>
2022-07-11 15:40:03 -0300

    Aplicaçao do titulo da pagina List

--- src/components/pages/List/index.jsx
+++ src/components/pages/List/index.jsx
@@ -1,10 +1,13 @@
 import PokeList from "../../organisms/pokeList"
-
+import './PokeCard.scss'
 const List = () => {
   return(
+    <>
+    <h1 className="PokeList__Title"> Pokedex </h1>
     <div className="PokeList__cards">
     <PokeList />
     </div>
+    </>
   )
 }
 
