732b273a021c335746dbd151abe83ed4f5865e58
xossiane <anejosi.lima@gmail.com>
2022-07-11 16:50:31 -0300

    CSS aplicado para a pokedex

--- src/components/pages/List/index.jsx
+++ src/components/pages/List/index.jsx
@@ -1,11 +1,20 @@
 import PokeList from "../../organisms/pokeList"
 import './PokeCard.scss'
+import logo from "../../../../public/assets/logo.png"
+import arrow from "../../../../public/assets/arrow.png"
+import list from "../../../../public/assets/list.png"
+
 const List = () => {
   return(
     <>
-    <h1 className="PokeList__Title"> Pokedex </h1>
+    <div className="PokeList">
+    <img src={logo} className="PokeList__logo"/>
+    <img src={arrow} className="PokeList__arrow" />
+    <img src={list} className="PokeList__list" />
+    <h1 className="PokeList__title"> Pokedex </h1>
     <div className="PokeList__cards">
     <PokeList />
+    </div>
     </div>
     </>
   )
