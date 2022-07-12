aa4fa2a552e35bf31f008dcce7e08f7745ea765e
xossiane <anejosi.lima@gmail.com>
2022-07-12 13:49:56 -0300

    Inserção da segunda tag de type com css aplicado e arrow com redirect para a homepage

--- src/components/molecules/pokemonCard/index.jsx
+++ src/components/molecules/pokemonCard/index.jsx
@@ -11,8 +11,9 @@ const PokeCard = ({id, name, img, type}) => {
          {/* < img src={img} /> */}
          <div className="pokeCard__type__card"> 
          <h2 className="pokeCard__type__title">{name}</h2>
-         {type.map(el => (<h2 className="pokeCard__type__tag">{el.type.name}</h2>
-         ))}
+         {type.map((el, index) => (<h2 className={`pokeCard__type__tag--${index}`}>{el.type.name}</h2>
+         ))
+         }
          <h2 className="pokeCard__type__id">#{id}</h2>
          <img className="pokeCard__type__tag__logo" src={pokecardlogo}/>
          <img className="pokeCard__type__tag__img" src={img}/>   
--- src/components/pages/List/index.jsx
+++ src/components/pages/List/index.jsx
@@ -5,6 +5,7 @@ import arrow from "../../../../public/assets/arrow.png"
 import list from "../../../../public/assets/list.png"
 import pikachu1 from "../../../../public/assets/pikachu1.png"
 import pikachu2 from "../../../../public/assets/pikachu2.png"
+import { Link } from "react-router-dom"
 
 const List = () => {
   return(
@@ -13,7 +14,9 @@ const List = () => {
     <img src={pikachu1} className="homePage__pikachu1" />
       <img src={pikachu2} className="homePage__pikachu2" />
     <img src={logo} className="PokeList__logo"/>
+    <Link className="PokeList__arrow__link" to="/">
     <img src={arrow} className="PokeList__arrow" />
+    </Link>
     <img src={list} className="PokeList__list" />
     <h1 className="PokeList__title"> Pokedex </h1>
     <div className="PokeList__cards">
