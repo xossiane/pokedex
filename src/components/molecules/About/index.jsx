39e8c14e5aa0c670808b87eb585a51e1ed6a0205
xossiane <anejosi.lima@gmail.com>
2022-07-12 16:08:43 -0300

    Esqueleto do about estático

--- src/components/molecules/About/index.jsx
+++ src/components/molecules/About/index.jsx
@@ -3,7 +3,11 @@ import './About.scss'
 function About(){
     return (
     <div className="About__container">
-     <p>hello</p>
+        <h2 className="About__title"> Bulbasaur </h2>
+        <div className="About__id">#001</div>
+        <div className="About__container__info">
+        
+     </div>
      </div>
     )
     }
